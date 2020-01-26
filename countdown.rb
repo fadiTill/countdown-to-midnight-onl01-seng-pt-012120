fe5bfe8e617e76bb80703ea56d24bf70613cdfee
fadimata tillman <fadi.tillman@gmail.com>
2020-01-26 23:52:38 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,8 +1,8 @@
 #write your code here
 
 def countdown
-  number = 0
-  while number < 10
-    puts "#{number} SECOND(S)!"+= 1
+  number = 10
+  while number > 0
+    puts "#{number} SECOND(S)!"-= 1
 end
 end
