5e312243a697164286919594d197dab37228554e
fadimata tillman <fadi.tillman@gmail.com>
2020-01-27 00:20:45 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -9,10 +9,10 @@ end
 "HAPPY NEW YEAR!"
 end
 
-def countdown_with_sleep(secs)
-secs(5)
-  while secs > 0
-    puts "#{secs} SECOND(S)!"
+def countdown_with_sleep(number)
+number = 10
+  while number > 0
+    puts "#{number} SECOND(S)!"
     number-= 1
     sleep(1)
     end
